fcde0a484c445dbe9eeb1d5c3e5dbdc57b5bbc01
davoszabo <davo.szabo@gmail.com>
2024-04-16 18:14:10 +0200

    Patching config

--- tmux-neovim/config/.config/nvim/lua/plugins/neo-tree.lua
+++ tmux-neovim/config/.config/nvim/lua/plugins/neo-tree.lua
@@ -8,6 +8,22 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            -- ".github",
+            -- ".gitignore",
+            -- "package-lock.json",
+          },
+          never_show = { ".git" },
+        },
+      },
+    })
     vim.keymap.set("n", "<leader>ee", ":Neotree source=filesystem toggle=true position=left<CR>", {})
     vim.keymap.set("n", "<leader>eb", ":Neotree buffers reveal float<CR>", {})
   end,
