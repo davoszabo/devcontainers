deb6d142e6e9c7131464086496958d5fe8e8f63e
davoszabo <davo.szabo@gmail.com>
2024-05-14 18:38:19 +0000

    Link host user directly into the container
    There will be no user creation inside the container during build,
    because the user directly linked into the container via docker-compose.
    This will enable user independent builds, which means version tagging
    for the images will be available soon!

--- tmux-neovim/entrypoint.sh
+++ tmux-neovim/entrypoint.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
+if [ ! -d ~/.tmux ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
 
-IFS=';' read -ra install_array <<< "$INSTALL"
+# Ubuntu bashrc
+cp /etc/skel/.bashrc ~/.bashrc-devcontainer
 
-for package in "${install_array[@]}"; do
-  # TODO: Error handling
-	~/install_packages/${package}.sh
-done
+# Uncomment the "force_color_prompt=yes" to show color in prompt
+sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc-devcontainer
+
+# Check if the .bashrc-devcontainer is referenced, and if not, write it into .bashrc
+if ! grep -wq ".bashrc-devcontainer" ~/.bashrc; then
+    cat >> ~/.bashrc <<EOF
+
+# Linking devcontainer bashrc
+if [ -f ~/.bashrc-devcontainer ]; then
+    source ~/.bashrc-devcontainer
+fi
+
+EOF
+fi
+
+# IFS=';' read -ra install_array <<< "$INSTALL"
+#
+# for package in "${install_array[@]}"; do
+#   # TODO: Error handling
+# 	~/install_packages/${package}.sh
+# done
 
 tail -f /dev/null # Keep container alive
